ccc34d3ed35c9c3cc9baeba1abe5459131b4561c
anvyden <anvyden@gmail.com>
2021-10-25 15:54:23 +0700

    created and finished dropdown.pug + scss.pug

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import * as $ from 'jquery'
 import '~/scss/scss.scss'
 import '~/common.blocks/UI-kit/logo-colorless/logo-colorless.js'
+import '~/common.blocks/UI-kit/form-elements/dropdown/dropdown.js'
 
