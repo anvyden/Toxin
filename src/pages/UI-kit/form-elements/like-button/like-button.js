c28c66e7703d7eb7317203049c4bbc7733f3f469
anvyden <anvyden@gmail.com>
2021-11-08 19:49:06 +0700

    add like-button and rate-button

--- src/pages/UI-kit/form-elements/like-button/like-button.js
+++ src/pages/UI-kit/form-elements/like-button/like-button.js
@@ -2,32 +2,54 @@ import * as $ from 'jquery'
 import './like-button-disabled.svg'
 import './like-button-enabled.svg'
 
-$(function (){
-  var likeToggleCounter = 0;
-  $('button[name="like"]').click(function(){
-    if (likeToggleCounter == 0) {
-      $('.like__img').attr('src', 'assets/img/like-button-enabled.svg')
-      $(this).toggleClass('like--active');
-      likeToggleCounter = 1;
-    } else {
-      $('.like__img').attr('src', 'assets/img/like-button-disabled.svg');
-      $(this).toggleClass('like--active');
-      likeToggleCounter = 0;
-    }
-  });
-});
+$(document).ready(function (){
 
-$(function (){
-  var likeToggleCounter = 0;
-  $('button[name="likeEnabled"]').click(function(){
-    if (likeToggleCounter == 0) {
-      $('.like__img').attr('src', 'assets/img/like-button-enabled.svg')
-      $(this).toggleClass('like--active');
-      likeToggleCounter = 1;
-    } else {
-      $('.like__img').attr('src', 'assets/img/like-button-disabled.svg');
-      $(this).toggleClass('like--active');
-      likeToggleCounter = 0;
-    }
+
+  // give function for like-button image id
+  $(function giveLikeBtnsImageId(){
+    let idNumber = 1;
+    $('.like__img').each(function(){
+      $(this).attr('id', `likeImg-${idNumber}`);
+      idNumber++;
+    })
   });
+  
+  // toggle function for like-buttons через addEventListener
+  const likeBtns = document.querySelectorAll('.like')
+  likeBtns.forEach(likeBtn => {
+    likeBtn.addEventListener('click', e => {
+
+      let currentTarget = e.currentTarget;
+      let imgId = $(currentTarget).children('img').attr('id');
+      let likeCounter = $(currentTarget).children('span').html();
+
+      $(currentTarget).toggleClass('like--active');
+      if ($(currentTarget).hasClass('like--active')){
+        likeCounter++;
+        $(`#${imgId}`).attr('src', 'assets/img/like-button-enabled.svg');
+        $(currentTarget).children('span').html(likeCounter);
+      } else {
+        likeCounter--;
+        $(`#${imgId}`).attr('src', 'assets/img/like-button-disabled.svg');
+        $(currentTarget).children('span').html(likeCounter);
+      }
+    })
+  })
+
+  // toggle function for like-buttons через click
+
+  // $(function (){
+  //   $('.like').click(function(e){
+  //     let likeBtnImageId = $(e.currentTarget).children('img').attr('id');
+  //     console.log(e)
+  //     console.log(likeBtnImageId)
+  //     $(this).toggleClass('like--active');
+  //     if ($(this).hasClass('like--active')){
+  //       $(`#${likeBtnImageId}`).attr('src', 'assets/img/like-button-enabled.svg')
+  //     } else {
+  //       $(`#${likeBtnImageId}`).attr('src', 'assets/img/like-button-disabled.svg');
+  //     }
+  //   })
+  // })
+
 });
