f766d4a82ad630304103c376e0f82f61779cdddb
anvyden <anvyden@gmail.com>
2021-12-13 17:45:24 +0700

    refacted pug's files structure in pages

--- webpack.config.js
+++ webpack.config.js
@@ -166,7 +166,7 @@ module.exports = {
     port: 8081,
     hot: isDev,
     watchContentBase: true,
-    index: 'colors-type.html'
+    index: 'form-elements.html'
   },
   devtool: isDev ? 'source-map' : false,
   plugins: plugins(),
