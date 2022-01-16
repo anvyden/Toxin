0f7c5cb7b0fdac9f600759e7249e7f661076b86e
anvyden <anvyden@gmail.com>
2022-01-17 03:24:39 +0700

    refacted toggle, like, rate buttons pug and scss files. Also refacted form-elements pug templates and scss file

--- src/templates/UI-kit/form-elements/like-button/like-button.js
+++ src/templates/UI-kit/form-elements/like-button/like-button.js
@@ -1,38 +1,22 @@
 class LikeButton {
   constructor() {
-    this.$el = document.querySelectorAll('.js-like-button__button')
-    this.getId()
+    this.$el = document.querySelectorAll('.js-like-button')
     this.like()
   }
 
-  // Присваиваю id картинкам
-  getId() {
-    let idValue = 0
-
-    this.$el.forEach((element) => {
-      $(element).children('img').attr('id', `likeImg-${idValue}`)
-      idValue += 1
-    })
-  }
-
-  // Toggle class active
   like() {
     this.$el.forEach((element) => {
       element.addEventListener('click', (event) => {
         const target = event.currentTarget
-        const imgId = $(target).children('img').attr('id')
-        let likeCounter = +$(target).children('span').html()
-
+        let likeCounter = +$(target).html()
         $(target).toggleClass('like-button__button--active')
 
         if ($(target).hasClass('like-button__button--active')) {
           likeCounter += 1
-          $(`#${imgId}`).attr('src', 'assets/img/like-button-enabled.svg')
-          $(target).children('span').html(likeCounter)
+          $(target).html(likeCounter)
         } else {
           likeCounter -= 1
-          $(`#${imgId}`).attr('src', 'assets/img/like-button-disabled.svg')
-          $(target).children('span').html(likeCounter)
+          $(target).html(likeCounter)
         }
       })
     })
