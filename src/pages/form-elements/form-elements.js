62b6bb1e2bdbfb247007c99f021dbc93c8cd04f6
anvyden <anvyden@gmail.com>
2022-01-22 03:44:21 +0700

    make range-slider block

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,3 +1,11 @@
 import formElements from 'pages/form-elements/form-elements.scss'
 import LikeButton from 'form-elements/like-button/like-button'
 import * as textField from 'form-elements/text-field/text-field'
+import RangeSlider from 'form-elements/range-slider/range-slider'
+
+const rangeSlider = new RangeSlider('js-range-slider', 'js-range-slider-price', {
+  min: 0,
+  max: 15000,
+  minStartPrice: 5000,
+  maxStartPrice: 10000,
+})
---
+++ src/templates/UI-kit/form-elements/range-slider/range-slider.js
@@ -0,0 +1,71 @@
+import noUiSlider from 'nouislider'
+
+class RangeSlider {
+  constructor(sliderId, priceSliderId, options) {
+    this.rangeSlider = document.getElementById(sliderId)
+    this.rangeSliderPrice = document.getElementById(priceSliderId)
+    this.options = options
+    this.cssClasses = noUiSlider.cssClasses
+    this.stylization()
+    this._init()
+  }
+
+  stylization() {
+    this.cssClasses.target += ' range-slider__target'
+    this.cssClasses.handle += ' range-slider__handle'
+    this.cssClasses.connect += ' range-slider__connect'
+    this.cssClasses.connects += ' range-slider__connects'
+  }
+
+  _init() {
+    const {
+      min,
+      max,
+      minStartPrice,
+      maxStartPrice,
+    } = this.options
+    if (this.rangeSlider) {
+      noUiSlider.create(this.rangeSlider, {
+        start: [minStartPrice, maxStartPrice],
+        step: 1,
+        connect: true,
+        range: {
+          min: [min],
+          max: [max],
+        },
+      })
+
+      this.rangeSlider.noUiSlider.on('update', (values) => {
+        this.rangeSliderPrice.innerHTML = `${Math.round(values[0])}₽ - ${Math.round(values[1])}₽`
+      })
+    }
+  }
+}
+
+export default RangeSlider
+
+// const rangeSlider = document.getElementById('js-range-slider')
+// const rangeSliderPrice = document.getElementById('js-range-slider-price')
+
+// // Стилизация слайдера
+// noUiSlider.cssClasses.target += ' range-slider__target'
+// noUiSlider.cssClasses.handle += ' range-slider__handle'
+// noUiSlider.cssClasses.connect += ' range-slider__connect'
+// noUiSlider.cssClasses.connects += ' range-slider__connects'
+
+// // Инициализация range-slider
+// if (rangeSlider) {
+//   noUiSlider.create(rangeSlider, {
+//     start: [5000, 10000],
+//     step: 1,
+//     connect: true,
+//     range: {
+//       min: [0],
+//       max: [15000],
+//     },
+//   })
+
+//   rangeSlider.noUiSlider.on('update', (values) => {
+//     rangeSliderPrice.innerHTML = `${Math.round(values[0])}₽ - ${Math.round(values[1])}₽`
+//   })
+// }
