7acb4066604963700d746f3a313c2377d9ced683
anvyden <anvyden@gmail.com>
2022-03-31 01:18:31 +0700

    init elements on rood-details page

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -1,1 +1,37 @@
 import roomDetails from '@pages/room-details/room-details.scss'
+import Dropdown from '@form-elements/dropdown/dropdown'
+import LikeButton from '@form-elements/like-button/like-button'
+import { Datepicker } from '@cards/datepicker/datepicker'
+import BookingCard from '@cards/booking-card/booking-card'
+import Menu from '@headers-footers/menu/menu'
+import Header from '@headers-footers/header/header'
+
+const roomInfo = {
+  roomNumber: '888',
+  roomPrice: '9990',
+  discount: '2179',
+  additionalServicesSum: '300',
+}
+
+const init = (function () {
+  return {
+    menu: new Menu(),
+    header: new Header(),
+    datepicker: new Datepicker(
+      {
+        datepickerSelectors: {
+          input: document.querySelector('.js-booking-card-input-1'),
+          inputSecond: document.querySelector('.js-booking-card-input-2'),
+          datepickerDropdowns: document.querySelectorAll('.js-booking-card-dropdown .dropdown__dropdown-default'),
+        },
+      },
+      roomInfo,
+    ),
+    dropdownGuests: new Dropdown('js-booking-card-guests', {
+      dropdownButtons: true,
+      combineTwoFirstItems: true,
+    }),
+    bookingCard: new BookingCard(roomInfo),
+    likeButtons: new LikeButton('js-like-button'),
+  }
+}())
--- webpack.config.js
+++ webpack.config.js
@@ -210,7 +210,7 @@ module.exports = {
     port: 8081,
     hot: isDev,
     watchContentBase: true,
-    index: 'form-elements.html',
+    index: 'room-details.html',
   },
   devtool: isDev ? 'source-map' : false,
   plugins: plugins(),
