6fcdab2273602738a57d9ac2a63ad5de60888470
anvyden <anvyden@gmail.com>
2022-07-15 03:01:34 +0700

    refactor: Added .stylelintrc.js, .prettierrc. Refactor postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,23 @@
+const devMode = process.env.NODE_ENV === 'development';
+
+const devOptions = [
+  require('autoprefixer'),
+];
+
+const productionOptions = [
+  require('autoprefixer'),
+  require('css-mqpacker'),
+  require('cssnano')({
+    preset: [
+      'default', {
+        discardComments: {
+          removeAll: true,
+        },
+      },
+    ],
+  }),
+];
+
 module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    require('css-mqpacker'),
-    require('cssnano')({
-      preset: [
-        'default', {
-          discardComments: {
-            removeAll: true,
-          }
-        }
-      ]
-    })
-  ]
-}
+  plugins: devMode ? devOptions : productionOptions,
+};
