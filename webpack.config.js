91368bc04f95124e6e62e37759053159f196c6e0
anvyden <anvyden@gmail.com>
2022-07-15 18:54:22 +0700

    refactor: refactor color and type mixins and styles. Also refactor colors-type page

--- webpack.config.js
+++ webpack.config.js
@@ -208,7 +208,7 @@ module.exports = {
     port: 8081,
     hot: isDev,
     watchContentBase: true,
-    index: 'search-room.html',
+    index: 'landing-page.html',
   },
   devtool: isDev ? 'source-map' : false,
   plugins: plugins(),
