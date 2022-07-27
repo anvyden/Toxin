d849a9f25dbb503e16ebecc5ea26c050182b95be
anvyden <anvyden@gmail.com>
2022-07-28 02:39:46 +0700

    refactor: refactor star-rating

--- src/components/star-rating/star-rating.js
+++ src/components/star-rating/star-rating.js
@@ -1,33 +1,33 @@
 class StarRating {
   constructor(selector, score) {
-    this.starRating = document.querySelector(`${selector}`)
-    this.score = score
-    this._render()
-  }
-
-  _setup() {
-    this.score = Math.round(this.score)
+    this.starRating = document.querySelector(`${selector}`);
+    this.score = score;
+    this._render();
   }
 
   _render() {
-    this._setup()
-    this.rateList = []
+    this._validate();
+    this.rateList = [];
 
-    for (let i = 1; i <= 5; i += 1) {
-      this.star = document.createElement('span')
-      this.star.classList.add('star-rating__star', 'material-icons')
-      if (i <= this.score) {
-        this.star.innerHTML = 'star'
+    for (let i = 0; i < 5; i++) {
+      this.star = document.createElement('span');
+      this.star.classList.add('star-rating__star', 'material-icons');
+      if (i < this.score) {
+        this.star.innerHTML = 'star';
       } else {
-        this.star.innerHTML = 'star_border'
+        this.star.innerHTML = 'star_border';
       }
-      this.rateList.push(this.star)
+      this.rateList.push(this.star);
     }
 
     this.rateList.forEach((item) => {
-      this.starRating.appendChild(item)
-    })
+      this.starRating.appendChild(item);
+    });
+  }
+
+  _validate() {
+    this.score = Math.round(this.score);
   }
 }
 
-export default StarRating
+export default StarRating;
