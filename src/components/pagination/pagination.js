0379c7f95f5658a83d7d13d402155f05af6babde
anvyden <anvyden@gmail.com>
2022-08-02 03:23:54 +0700

    refactor: refactor features, comments components and refactor pagination js with eslint rules

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -42,7 +42,7 @@ class Pagination {
 
         arrow.addEventListener(
           'pointerdown',
-          this._handleArrowPointerDown.bind(this)
+          this._handleArrowPointerDown.bind(this),
         );
         arrow.addEventListener('keydown', this._handleArrowKeyDown.bind(this));
 
@@ -53,7 +53,7 @@ class Pagination {
         if (value !== '...') {
           item.addEventListener(
             'pointerdown',
-            this._handleItemPointerDown.bind(this)
+            this._handleItemPointerDown.bind(this),
           );
           item.addEventListener('keydown', this._handleItemKeyDown.bind(this));
         }
@@ -109,60 +109,54 @@ class Pagination {
   _getListValues(dataValue, countOfPages) {
     const values = [];
 
-    for (let i = 0; i < countOfPages; i++) {
+    for (let i = 0; i < countOfPages; i += 1) {
       values.push(i + 1);
     }
 
     const correctValues = values.map((value) => {
-      const isMoreDataValueByTwo =
-        countOfPages - (dataValue + 2) > 2 && value > dataValue + 2;
-      const isLessDataValueByTwo =
-        dataValue - 2 - 1 > 2 && value < dataValue - 2;
-      const isDiffersByLessThanTwo =
-        dataValue + 2 >= value && value >= dataValue - 2;
+      const isMoreDataValueByTwo = countOfPages - (dataValue + 2) > 2 && value > dataValue + 2;
+      const isLessDataValueByTwo = dataValue - 2 - 1 > 2 && value < dataValue - 2;
+      const isDiffersByLessThanTwo = dataValue + 2 >= value && value >= dataValue - 2;
 
       if (value === 1 || value === countOfPages) {
         return value;
-      } else if (isMoreDataValueByTwo) {
+      } if (isMoreDataValueByTwo) {
         return '...';
-      } else if (isLessDataValueByTwo) {
+      } if (isLessDataValueByTwo) {
         return '...';
-      } else if (isDiffersByLessThanTwo) {
-        return value;
-      } else {
+      } if (isDiffersByLessThanTwo) {
         return value;
       }
+      return value;
     });
 
     const firstHalf = correctValues.slice(0, dataValue);
     const secondHalf = correctValues.slice(dataValue, countOfPages);
 
-    let filteredValues = [
+    this.filteredValues = [
       ...firstHalf.filter((item, index) => firstHalf.indexOf(item) === index),
       ...secondHalf.filter((item, index) => secondHalf.indexOf(item) === index),
     ];
 
-    if (dataValue !== 1) filteredValues = ['arrow_back', ...filteredValues];
-    if (dataValue !== countOfPages)
-      filteredValues = [...filteredValues, 'arrow_forward'];
+    if (dataValue !== 1) this.filteredValues = ['arrow_back', ...this.filteredValues];
+    if (dataValue !== countOfPages) this.filteredValues = [...this.filteredValues, 'arrow_forward'];
 
-    return filteredValues;
+    return this.filteredValues;
   }
 
   _createList() {
-    const list = document.createElement('ul');
-    list.classList.add('pagination__list');
+    this.list = document.createElement('ul');
+    this.list.classList.add('pagination__list');
 
-    return list;
+    return this.list;
   }
 
   _createItem(value) {
-    const item = document.createElement('li');
-    item.classList.add('pagination__item');
+    this.item = document.createElement('li');
+    this.item.classList.add('pagination__item');
 
-    if (this.dataValue === value)
-      item.classList.add('pagination__item--active');
-    if (value === '...') item.classList.add('pagination__item--disabled');
+    if (this.dataValue === value) this.item.classList.add('pagination__item--active');
+    if (value === '...') this.item.classList.add('pagination__item--disabled');
 
     const link = document.createElement('a');
     link.classList.add('pagination__item-link');
@@ -171,14 +165,14 @@ class Pagination {
 
     if (value !== '...') link.tabIndex = 0;
 
-    item.appendChild(link);
+    this.item.appendChild(link);
 
-    return item;
+    return this.item;
   }
 
   _createArrow(value) {
-    const arrow = document.createElement('li');
-    arrow.classList.add('pagination__item', 'pagination__item--with-arrow');
+    this.arrow = document.createElement('li');
+    this.arrow.classList.add('pagination__item', 'pagination__item--with-arrow');
 
     const arrowLink = document.createElement('a');
     arrowLink.classList.add('pagination__item-link', 'material-icons');
@@ -187,32 +181,32 @@ class Pagination {
 
     if (value === 'arrow_forward') {
       arrowLink.classList.add(
-        ...['pagination__arrow-prev', 'js-pagination__arrow-prev']
+        ...['pagination__arrow-prev', 'js-pagination__arrow-prev'],
       );
       arrowLink.dataset.type = 'next';
     } else {
       arrowLink.classList.add(
-        ...['pagination__arrow-next', 'js-pagination__arrow-next']
+        ...['pagination__arrow-next', 'js-pagination__arrow-next'],
       );
       arrowLink.dataset.type = 'prev';
     }
 
-    arrow.appendChild(arrowLink);
+    this.arrow.appendChild(arrowLink);
 
-    return arrow;
+    return this.arrow;
   }
 
   _createDescription() {
     const { itemsPerPage } = this.options;
 
-    const description = document.createElement('span');
-    description.classList.add('pagination__description');
+    this.description = document.createElement('span');
+    this.description.classList.add('pagination__description');
 
     const startItem = itemsPerPage * (this.dataValue - 1) + 1;
     const endItem = itemsPerPage * this.dataValue;
-    description.textContent = `${startItem} – ${endItem} из 100+ вариантов аренды`;
+    this.description.textContent = `${startItem} – ${endItem} из 100+ вариантов аренды`;
 
-    return description;
+    return this.description;
   }
 }
 
