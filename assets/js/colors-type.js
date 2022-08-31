fb3b89b3486b037854da4da902e3ac17779ff4fd
anvyden <anvyden@gmail.com>
2022-08-31 15:54:39 +0700

    Updates

--- assets/js/colors-type.js
+++ assets/js/colors-type.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[849],{9001:()=>{}},e=>{e.O(0,[216],(()=>{return s=9001,e(e.s=s);var s}));e.O()}]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[849],{8686:()=>{}},s=>{var e;e=8686,s(s.s=e)}]);
