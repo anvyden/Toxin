40f7333b5b43a949c52cee513f7dd97cdeebd84a
anvyden <anvyden@gmail.com>
2022-10-11 21:12:29 +0700

    fix: remove text-field.js. Added inputmask in libs folder which initing in text-field index.js. remove datepicker.pug

--- src/components/text-field/index.js
+++ src/components/text-field/index.js
@@ -1,4 +1,8 @@
 import '~/components/text';
+import dateMask from '@libs/inputmask';
 
-import './text-field';
 import './text-field.scss';
+
+$(document).ready(() => {
+  dateMask.mask('.js-masked-text-field');
+});
